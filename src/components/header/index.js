6210876614dfe202b49b7d9b6ba08d1ab7cb4ac6
Stanislau Dykomenko <stanislau.dykomenko@effective-soft.com>
2022-08-26 10:21:13 +0300

    deleted useless

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,9 +5,6 @@ import { useAuth } from "../../hooks/useAuth";
 import Navigation from "../Navigation";
 
 import userPhoto from "../../assets/images/user-photo.svg";
-import faqImage from "../../assets/images/faq.svg";
-import bellImage from "../../assets/images/bell.svg";
-import optionsImage from "../../assets/images/options.svg";
 
 import "./styled.css";
 
@@ -25,9 +22,6 @@ export default function Header() {
             <div className="header__inner">
                 <section className="header__inner-top">
                     <div className="header__inner-top__title">
-                        {/* <label className="menu__btn">
-                            <span></span>
-                        </label> */}
                         <Link to="/">
                             <h1 className="header__inner-top__title-text">
                                 Диспетчерская УО &#171;ВГТУ&#187;
@@ -36,29 +30,6 @@ export default function Header() {
                     </div>
                     {user && (
                         <div className="header__inner-top__content">
-                            {/* <div className="header__inner-top__content-settings">
-                            <Link to="/">
-                                <img
-                                    className="top__content-settings__faq"
-                                    src={faqImage}
-                                    alt="FAQ"
-                                />
-                            </Link>
-                            <Link to="/">
-                                <img
-                                    className="top__content-settings__bell"
-                                    src={bellImage}
-                                    alt="BELL"
-                                />
-                            </Link>
-                            <Link to="/">
-                                <img
-                                    className="top__content-settings__options"
-                                    src={optionsImage}
-                                    alt="SETTINGS"
-                                />
-                            </Link>
-                        </div> */}
                             <div className="header__inner-top__content-user">
                                 <img
                                     className="top__content-user__img"
